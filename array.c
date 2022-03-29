a7d4b141c2bc8e9b2e70905c5441e0967cdfe6bf
sharwaridaware <dawaresharwari@gmail.com>
2022-03-29 10:56:02 +0530

    change1

--- array.c
+++ array.c
@@ -5,5 +5,6 @@ int i;
 int a[5]={1,2,3,4,5};
 print("Enter position of element you want to print")
 scanf("%d",&i);
-print("Value at %d position is %d,i,a[i];
+printf("Value at %d position is %d,i,a[i];
+printf("sharwari");		
 }
